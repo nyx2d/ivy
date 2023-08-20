5116cfb1df803e8bfe34e3fed842dca30af46e99
nyx <113071411+nyx2d@users.noreply.github.com>
2023-08-20 13:07:22 -0700

    clean up discovery logic

--- go.mod
+++ go.mod
@@ -5,11 +5,13 @@ go 1.20
 require (
 	github.com/fxamacker/cbor/v2 v2.5.0
 	github.com/hashicorp/mdns v1.0.5
+	github.com/samber/lo v1.38.1
 )
 
 require (
 	github.com/miekg/dns v1.1.41 // indirect
 	github.com/x448/float16 v0.8.4 // indirect
+	golang.org/x/exp v0.0.0-20220303212507-bbda1eaf7a17 // indirect
 	golang.org/x/net v0.0.0-20210410081132-afb366fc7cd1 // indirect
-	golang.org/x/sys v0.0.0-20210330210617-4fbd30eecc44 // indirect
+	golang.org/x/sys v0.0.0-20211019181941-9d821ace8654 // indirect
 )
--- network/client.go
+++ network/client.go
@@ -6,50 +6,53 @@ import (
 	"time"
 
 	"github.com/hashicorp/mdns"
+	"github.com/samber/lo"
+	_ "github.com/samber/lo"
 )
 
-const scanPeriod = 2 * time.Second
+const scanPeriod = 10
+const scanTimeout = 2 * time.Second
+const scanBufferSize = 256
 
 func FindPeers(peerID string) {
 	ticker := time.NewTicker(scanPeriod)
 	for ; true; <-ticker.C {
-		entries := make(chan *mdns.ServiceEntry, 256)
+		entriesChan := make(chan *mdns.ServiceEntry, scanBufferSize)
 		err := mdns.Query(&mdns.QueryParam{
 			Service:     serviceName,
 			Domain:      "local",
-			Timeout:     scanPeriod,
-			Entries:     entries,
+			Timeout:     scanTimeout,
+			Entries:     entriesChan,
 			DisableIPv6: true,
 		})
 		if err != nil {
 			log.Fatal(err)
 		}
 
-	loop:
-		for {
-			select {
-			case entry := <-entries:
-				if len(entry.InfoFields) < 2 || entry.InfoFields[0] != serviceName {
-					continue // not a node, extra guard against bad mDNS DNS-SD behavior (looking at you roku)
-				}
-				if entry.InfoFields[1] == peerID {
-					continue // skip self
-				}
+		entries, _, _, ok := lo.BufferWithTimeout(entriesChan, scanBufferSize, scanTimeout)
+		if !ok {
+			log.Println("issue with buffer timeout")
+			continue
+		}
 
-				addr := &net.TCPAddr{IP: entry.AddrV4, Port: entry.Port}
-				if entry.Info != peerID && !connManager.Active(addr) {
-					go func() {
-						peer, err := NewServerPeer(addr)
-						if err != nil {
-							log.Printf("error connecting to peer: %s\n", err)
-							return
-						}
-						peer.Handle()
-					}()
-				}
+		for _, entry := range entries {
+			if len(entry.InfoFields) < 2 || entry.InfoFields[0] != serviceName {
+				continue // not a node, extra guard against bad mDNS DNS-SD behavior (looking at you roku)
+			}
+			if entry.InfoFields[1] == peerID {
+				continue // skip self
+			}
 
-			case <-time.After(scanPeriod):
-				break loop
+			addr := &net.TCPAddr{IP: entry.AddrV4, Port: entry.Port}
+			if entry.Info != peerID && !connManager.Active(addr) {
+				go func() {
+					peer, err := NewServerPeer(addr)
+					if err != nil {
+						log.Printf("error connecting to peer: %s\n", err)
+						return
+					}
+					peer.Handle()
+				}()
 			}
 		}
 	}
